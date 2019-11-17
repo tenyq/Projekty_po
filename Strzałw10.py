f2b7076f9001350564698522659a6796fd5e1028
SieronW <56339286+tenyq@users.noreply.github.com>
2019-11-17 17:08:15 +0100

    Add files via upload
    5 prob na strzał w  losowa liczbe

---
+++ Strzałw10.py
@@ -0,0 +1,25 @@
+import random
+
+liczb = random.randint(0, 100, )
+
+liczba = int(input("wybierz liczbe z przedzialu od 1 do 100: "))
+prob = 1
+
+while liczba != liczb:
+
+    if liczba > liczb:
+
+        print("Twoja liczba jest wieksza, sproboj jeszcze raz")
+    elif liczba < liczb:
+
+        print("Twoja liczba jest mniejsza, sproboj jeszcze raz")
+    liczba = int(input("wybierz liczbe z przedzialu od 1 do 100: "))
+    prob += 1
+    if prob == 5:
+
+        print("wykorzystales 5 prob, przegrywasz...")
+        break
+    else:
+        print("brawo zgadles, twoja liczba prob to :", prob, )
+
+input("\n\nAby zakonczyc pogram,nacisnij enter.")
