4440c2f2012601e8cbaae1ca84f0e85b3b3fd9e0
tenyq <sieronwojtek@gmail.com>
2020-01-14 20:48:31 +0100

    Dodanie:  zadawania losowego damage z przedzialu 1-5

--- prostagra.py
+++ prostagra.py
@@ -1,13 +1,15 @@
+import random
+
 print("Jestes rycerzem i masz 10 punktow zycia.")
 print("Naciera na ciebie grupa przeciwnikow.")
-print("Pogromienie kazdego z nich zabiera 2 punkty zycia.\n")
+print("Pogromienie kazdego z nich zabiera od 1 do 5 punktow zycia.\n")
 
 health = 10
 trolls = 0
-damage = 2
 
 while health > 0:
     trolls += 1
+    damage = random.randint(1, 5, )
     health -= damage
 
     print("Bohater pokonuje przeciwnika, " \
@@ -17,4 +19,3 @@ print("Twój bohater walczył dzielnie i pokonał", trolls, "przeciwnikow.")
 print("Lecz niestety Twój bohater już nie żyje.")
 
 input("\n\nAby zakończyć program, naciśnij klawisz Enter.")
-
