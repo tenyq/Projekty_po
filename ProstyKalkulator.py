77bd52ef1b484deb0c83c6741ec28022ea68c376
tenyq <sieronwojtek@gmail.com>
2020-01-14 21:54:10 +0100

    Dodanie mnozenia i dzielenia

--- ProstyKalkulator.py
+++ ProstyKalkulator.py
@@ -2,11 +2,20 @@ class Calculator():
     def dodaj(self, a, b):
         wynik = a + b
         print(wynik)
+
     def odejmij(self, a, b):
         wynik = a - b
         print(wynik)
 
+    def podziel(self, a, b):
+        wynik = a / b
+        print(wynik)
+
+    def pomnoz(self, a, b):
+        print(a * b)
 
 calc = Calculator()
-calc.odejmij(5,4)
-calc.dodaj(5,5)
+calc.odejmij(5, 4)
+calc.dodaj(5, 5)
+calc.podziel(5, 5)
+calc.pomnoz(5, 5)
